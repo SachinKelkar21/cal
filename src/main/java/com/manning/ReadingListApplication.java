6be47cbb1064a39355147cbcf7c566e4240aed11
SachinKelkar21 <sachin.kelkar79@gmail.com>
2022-07-10 12:47:54 +0530

    add comment in main class

--- src/main/java/com/manning/ReadingListApplication.java
+++ src/main/java/com/manning/ReadingListApplication.java
@@ -2,7 +2,7 @@ package com.manning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*This is main class*/
 @SpringBootApplication
 public class ReadingListApplication {
 
